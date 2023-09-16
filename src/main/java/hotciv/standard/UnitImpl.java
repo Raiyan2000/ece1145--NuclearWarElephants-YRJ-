61e9d5c9b3df2b13652d07fc002b0b3094468f7f
Raiyan2000 <77954118+Raiyan2000@users.noreply.github.com>
2023-09-16 17:50:10 -0400

    Added UnitImpl.java file

---
+++ src/main/java/hotciv/standard/UnitImpl.java
@@ -0,0 +1,33 @@
+package hotciv.standard;
+
+import hotciv.framework.*;
+public class UnitImpl implements Unit {
+
+    public String getTypeString(){
+        return null;
+    }
+
+
+
+    public Player getOwner()
+    {
+        return null;
+    }
+
+
+    public int getMoveCount()
+    {
+        return 0;
+    }
+
+
+    public int getDefensiveStrength()
+    {
+        return 0;
+    }
+
+    public int getAttackingStrength()
+    {
+        return 0;
+    }
+}
